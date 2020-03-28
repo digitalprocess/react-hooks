25d2eccb086dbac1ac6e8909ea66c96b93c94236
frontendista <csdelreal@gmail.com>
2020-03-28 15:24:13 -0500

    Fetching Data With useEffect

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, {useRef, createContext, useMemo} from 'react'
+import React, { useState, useEffect, useRef, createContext } from 'react'
 import {useTitleInput} from './hooks/useTitleInput'
 
 import './App.css'
 
 import Toggle from './Toggle'
-import Counter from './Counter'
 
 export const UserContext = createContext()
 
 const App = () => {
 	const [name, setName] = useTitleInput('')
 	const ref = useRef()
+	const [dishes, setDishes] = useState([])
 
-	const reverseWord = word => {
-		return word
-			.split('')
-			.reverse()
-			.join('')
+	const fetchData = async () => {
+		console.log('ran')
+		const res = await fetch('https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes')
+		const data = await res.json()
+		setDishes(data)
 	}
 
-	const title = 'React Hooks'
-	const TitleReversed = useMemo(() => reverseWord(title), [title])
+	useEffect(() => {
+		fetchData();
+	}, [name]); // this tells useEffect what to watch for when it changes
 
 	return (
+
 		<UserContext.Provider
 			value={{
 				user: true
@@ -32,18 +34,32 @@ const App = () => {
 				<h1
 					onClick={() => ref.current.classList.toggle('new-class')}
 				>
-					{TitleReversed}
+					React Hooks
 				</h1>
 				<Toggle>
 					<h2>Hello!</h2>
 				</Toggle>
-				<Counter />
 				<form onSubmit={ev => {
 					ev.preventDefault()
 				}}>
 					<input type="text" onChange={ev => setName(ev.target.value)} value={name} />
 					<button type="submit">Submit</button>
 				</form>
+
+				{dishes.map(dish => (
+					<article
+						key={dish.name}
+						className="dish-card dish-card--withImage"
+					>
+						<h3>{dish.name}</h3>
+						<p>{dish.desc}</p>
+						<div className="ingredients">
+							{dish.ingredients.map(ingredient => (
+								<span key={ingredient}>{ingredient}</span>
+							))}
+						</div>
+					</article>
+				))}
 			</div>
 		</UserContext.Provider>
 	)
