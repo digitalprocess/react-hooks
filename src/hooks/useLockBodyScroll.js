cfe09d61812d84d67cebd62ce066d80b388fc9a7
frontendista <csdelreal@gmail.com>
2020-03-28 18:02:58 -0500

    hook cleanup

--- src/hooks/useLockBodyScroll.js
+++ src/hooks/useLockBodyScroll.js
@@ -2,6 +2,13 @@ import { useLayoutEffect } from 'react'
 
 export function useLockBodyScroll() {
 	useLayoutEffect(() => {
+		const originalOverflow = window.getComputedStyle(document.body).overflow
+		console.log(originalOverflow)
 		document.body.style.overflow = 'hidden'
+
+		//cleanup
+		return () => {
+			document.body.style.overflow = originalOverflow
+		};
 	}, [])
 }
