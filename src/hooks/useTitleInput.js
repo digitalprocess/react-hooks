6d120a8baa6d806073c7443dec1d3014e71a776a
frontendista <csdelreal@gmail.com>
2020-03-28 14:05:35 -0500

    useDebugValue For Custom Hook Libraries

--- src/hooks/useTitleInput.js
+++ src/hooks/useTitleInput.js
@@ -1,10 +1,11 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect, useDebugValue} from 'react'
 
 function useTitleInput(initalValue) {
 	const [value, setValue] = useState(initalValue)
 	useEffect(() => {
 		document.title = value
 	})
+	useDebugValue(value.length > 0 ? 'Full' : 'Empty')
 	return [value, setValue]
 }
 
