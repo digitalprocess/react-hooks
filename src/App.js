614bfd708f0f7206533270ec346d40b22d07f9bd
frontendista <csdelreal@gmail.com>
2020-03-27 20:59:15 -0500

    usState to clear form values

--- src/App.js
+++ src/App.js
@@ -8,10 +8,24 @@ const App = () => {
 	return (
 		<div className="main-wrapper">
 			<h1>React Hooks</h1>
-			<input type="text" onChange={ev => setName(ev.target.value)} value={name} />
-			<Toggle />
+
+			<Toggle>
+				<h2>Hello!</h2>
+			</Toggle>
+			<form onSubmit={ev => {
+				ev.preventDefault()
+				formSubmt(name, setName)
+			}}>
+				<input type="text" onChange={ev => setName(ev.target.value)} value={name} />
+				<button>Submit</button>
+			</form>
 		</div>
 	)
+}
+
+const formSubmt = (value, setValue) => {
+	console.log('email sent to ' + value + '!')
+	setValue('')
 }
 
 export default App
--- src/Toggle.js
+++ src/Toggle.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 
-const Toggle = () => {
+const Toggle = ({children}) => {
 	const [isToggled, setIsToggled] = useState(false)
 	return (
 		<div>
 			<button onClick={() => setIsToggled(!isToggled)}>Toggle</button>
-			{isToggled && <h2>Hello</h2>}
+			{isToggled && children}
 		</div>
 	)
 }
