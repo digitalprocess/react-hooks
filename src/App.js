6513d8146491b1699774f6be77055263f155ede6
frontendista <csdelreal@gmail.com>
2020-03-27 20:26:29 -0500

    useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
 
 const App = () => {
+	const [name, setName] = useState('')
+
 	return (
 		<div className="main-wrapper">
-			<h1>Title</h1>
+			<h1>React Hooks</h1>
+			<input type="text" onChange={ev => setName(ev.target.value)} value={name} />
 		</div>
-	);
-};
+	)
+}
 
-export default App;
+export default App
